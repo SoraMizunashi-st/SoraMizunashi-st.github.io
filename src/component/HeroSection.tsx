9c2812d260b7993e390ffd9afe4b2ee7dab3fa48
usagi0419 <shuiwukong6@gmail.com>
2025-10-27 14:27:53 +0900

    re Test Lazyload | 2025/10/27 14:27

--- src/component/HeroSection.tsx
+++ src/component/HeroSection.tsx
@@ -5,7 +5,12 @@ import styles from './HeroSection.module.css';
 export default function HeroSection() {
     return (
         <section className={styles.hero}>
-          <img src = "/Hero/HeroBackground.jpg" loading="lazy"/>
+          <img  src = "/Hero/HeroBackground.jpg"
+                loading="lazy"
+                alt = "heroBackGround"
+                className="hero-preload-img"
+          />
+
           <h1 className={styles.heroTitle}>
             StellaLabの保管庫
           </h1>
